3310df8d309c15ba5422cfb48cfe1ec4a6ef3c5c
sgonzalezu <74781181+sgonzalezu@users.noreply.github.com>
2025-03-08 14:01:58 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.getElementById("predictionForm").addEventListener("submit", async function(event) {
     event.preventDefault();
     
-    const input1 = document.getElementById("input1").value;
-    const input2 = document.getElementById("input2").value;
+    const data = {
+        HomePlanet: document.getElementById("HomePlanet").value,
+        CryoSleep: document.getElementById("CryoSleep").value.toLowerCase() === "true",
+        Age: parseFloat(document.getElementById("Age").value),
+        VIP: document.getElementById("VIP").value.toLowerCase() === "true",
+        RoomService: parseFloat(document.getElementById("RoomService").value),
+        FoodCourt: parseFloat(document.getElementById("FoodCourt").value),
+        ShoppingMall: parseFloat(document.getElementById("ShoppingMall").value),
+        Spa: parseFloat(document.getElementById("Spa").value),
+        VRDeck: parseFloat(document.getElementById("VRDeck").value)
+    };
     
     const resultElement = document.getElementById("result");
     resultElement.textContent = "Obteniendo predicción...";
@@ -11,15 +20,15 @@ document.getElementById("predictionForm").addEventListener("submit", async funct
         const response = await fetch("https://prediction-f2cq.onrender.com/predict", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ input1, input2 })
+            body: JSON.stringify(data)
         });
         
         if (!response.ok) {
             throw new Error("Error en la respuesta del servidor");
         }
         
-        const data = await response.json();
-        resultElement.textContent = `Predicción: ${data.prediction}`;
+        const responseData = await response.json();
+        resultElement.textContent = `Predicción de Transported: ${responseData.prediction}`;
     } catch (error) {
         resultElement.textContent = "Error al obtener la predicción.";
         console.error("Error:", error);
